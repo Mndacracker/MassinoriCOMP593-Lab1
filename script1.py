6a31bc62e6e1b4f63ff98a51e82b9a0aed74df13
Mndacracker <mnori@flemingcollege.ca>
2023-01-18 13:27:24 -0500

    modify both scripts
    revert

--- script1.py
+++ script1.py
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 # Print lines from zen of python
 print("Simple is better than complex.")
+=======
+print("Simple is better than complex.")
 print("If the implementation is hard to explain, it's a bad idea.")
+>>>>>>> parent of 69720e9 (added comment to script1.py)
--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
+# Print lines from zen of python
 print("Beautiful is better than ugly.")
 print("Flat is better than nested.")
