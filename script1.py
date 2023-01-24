350fe61a09487fbd2cbaeb6c8ec643c195e49177
Mndacracker <mnori@flemingcollege.ca>
2023-01-24 11:09:09 -0500

    Modified both files in the repo clone
    -Deleted a print from script 1
    -Added a print for script 2

--- script1.py
+++ script1.py
@@ -1,6 +1,5 @@
 <<<<<<< HEAD
 # Print lines from zen of python
-print("Simple is better than complex.")
 =======
 print("Simple is better than complex.")
 print("If the implementation is hard to explain, it's a bad idea.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Print lines from zen of python
 print("Beautiful is better than ugly.")
 print("Flat is better than nested.")
+print("lol this stesssing so much work.")
